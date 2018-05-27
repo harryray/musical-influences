6ac02d4d772ae67c87810ceae77696a7edcff912
Harry Ray <harryraywebdev@gmail.com>
2018-05-27 19:21:29 +0100

    Update to homepage

--- index.php
+++ index.php
@@ -12,51 +12,53 @@
 <section>
 <form action="/inc/form_handler.php" name="artist_search" class="center-center-absolute">
 	<h1>Enter an artist below:</h1>
-	<input id="artist_name" name="artist_name" placeholder="Go ahead - type something..." type="text" /><input id="artist_submit" name="artist_submit" value="Punch it, Chewie!" type="submit" />
+	<input id="artist_name" name="artist_name" placeholder="Go ahead - type something..." type="text" /><input id="artist_submit" name="artist_submit" value="Search!" type="submit" />
 </form>
 </section>
 <section>
-<h2>Here's [NAME]'s influences</h2>
+<h2>Here's [ARTIST]'s influences</h2>
 <div id="chosen-artist">
-	<img src="" alt="[NAME]" />
-	<h3>[NAME]</h3>
+	<img src="" alt="[ARTIST]" />
+	<h3>[ARTIST]</h3>
 </div>
 <div id="influence-cards">
 	<div class="influence-card">
-		<img src="" alt="[NAME]" />
-		<h3>[NAME]</h3>
+		<img src="" alt="[ARTIST]" />
+		<h3>[ARTIST]</h3>
 		<div class="top-track">
 		</div>
 	</div>
 	<div class="influence-card">
-		<img src="" alt="[NAME]" />
-		<h3>[NAME]</h3>
+		<img src="" alt="[ARTIST]" />
+		<h3>[ARTIST]</h3>
 		<div class="top-track">
 		</div>
 	</div>
 	<div class="influence-card">
-		<img src="" alt="[NAME]" />
-		<h3>[NAME]</h3>
+		<img src="" alt="[ARTIST]" />
+		<h3>[ARTIST]</h3>
 		<div class="top-track">
 		</div>
 	</div>
 	<div class="influence-card">
-		<img src="" alt="[NAME]" />
-		<h3>[NAME]</h3>
+		<img src="" alt="[ARTIST]" />
+		<h3>[ARTIST]</h3>
 		<div class="top-track">
 		</div>
 	</div>
 	<div class="influence-card">
-		<img src="" alt="[NAME]" />
-		<h3>[NAME]</h3>
+		<img src="" alt="[ARTIST]" />
+		<h3>[ARTIST]</h3>
 		<div class="top-track">
 		</div>
 	</div>
 	<div class="influence-card">
-		<img src="" alt="[NAME]" />
-		<h3>[NAME]</h3>
+		<img src="" alt="[ARTIST]" />
+		<h3>[ARTIST]</h3>
 		<div class="top-track">
 		</div>
 	</div>
 </div>
 </section>
+
+<!-- http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=[ARTIST]&api_key=95be8d664959f5402c7f75142e65f6ec -->
