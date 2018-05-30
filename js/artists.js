25ad8811f8591ec8fa034ac9126214ecc7f80524
Harry Ray <harryraywebdev@gmail.com>
2018-05-30 21:22:29 +0100

    Update to js
    Moved the URLs over to https:// links, so that they actually work.

--- js/artists.js
+++ js/artists.js
@@ -15,8 +15,8 @@ angular.module('musicalInfluences',[])
 		e.preventDefault();
 		console.log('Submit detected');
 		var req = new XMLHttpRequest();
-		req.open("GET","http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=" + $scope.artistName + "&api_key=95be8d664959f5402c7f75142e65f6ec");
-		console.log(req.open("GET","http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=" + $scope.artistName + "&api_key=95be8d664959f5402c7f75142e65f6ec"));
+		req.open("GET","https://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=" + $scope.artistName + "&api_key=95be8d664959f5402c7f75142e65f6ec");
+		console.log(req.open("GET","https://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=" + $scope.artistName + "&api_key=95be8d664959f5402c7f75142e65f6ec"));
 		
 		req.send(null);
 		console.log($scope.artistName);
