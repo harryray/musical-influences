5ada558504351d82065c2843ae67d84ade7ccda6
Harry Ray <harryraywebdev@gmail.com>
2018-06-02 22:19:05 +0100

    Integrate new controller (skinController)

--- js/skin_selector.js
+++ js/skin_selector.js
@@ -1,1 +1,6 @@
-skin_selector
+angular.module('musicalInfluences',[]).controller('skinController',function($scope){
+	// When Skin Selector changes, fetch relevant stylesheet
+	$scope.updateSkin = function(){
+		
+	}
+})
